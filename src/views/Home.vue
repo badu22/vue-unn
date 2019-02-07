e2f99a851309ba8801d29fea1bfd53f1b4ce3918
badu <marko@nivas.hr>
2019-02-07 22:03:56 +0100

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HomePageComponent msg="Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HomePageComponent from '@/components/HomePageComponent.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HomePageComponent
+  }
+}
+</script>
